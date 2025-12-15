a6f6935be2c435be3b588759cd06fa557b5931c4
hariharan-vp-14 <hhvp14@gmail.com>
2025-12-15 23:25:33 +0530

    Fix Tailwind config and redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
 
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// import { PlayerProvider } from "./context/PlayerContext";
+// import "./index.css"; // ✅ THIS IS CRITICAL
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <PlayerProvider>
+//         <App />
+//       </PlayerProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { PlayerProvider } from "./context/PlayerContext";
-import "./index.css"; // ✅ THIS IS CRITICAL
+import "./index.css"; // ✅ REQUIRED
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
